d5eac21ad88e48d28ed5885702b844d713138cad
adhxabre <abeldustin06@gmail.com>
2023-05-19 10:29:53 +0700

    day-4

---
+++ js/blog.js
@@ -0,0 +1,62 @@
+// let namaSiswa1 = "Tamaro";
+// let namaSiswa2 = "Robby";
+// let namaSiswa3 = "Puja";
+
+// console.log(namaSiswa1);
+// console.log(namaSiswa2);
+// console.log(namaSiswa3);
+
+// array
+// let namaSiswa = ["Tamaro", "Robby", "Puja", "Rendi"];
+// console.log(namaSiswa);
+// console.log(namaSiswa[0]);
+// console.log(namaSiswa[2]);
+
+// object
+// let nama = "Badriana";
+// let alamat = "Tangerang Selatan";
+// let umur = 24;
+
+// console.log(nama);
+// console.log(alamat);
+// console.log(umur);
+
+// let dataPersonal1 = {
+//   nama: "Badriana",
+//   alamat: "Tangerang Selatan",
+//   umur: 24,
+// };
+
+// let dataPersonal2 = {
+//   nama: "Puan Maharani",
+//   alamat: "Jakarta Barat",
+//   umur: 48,
+// };
+
+// let dataPersonal3 = {
+//   nama: "Joko Widodo",
+//   alamat: "Solo",
+//   umur: 53,
+// };
+
+// let dataPersonal4 = {
+//   nama: "Megawati Soekarnoputri",
+//   alamat: "Jakarta Timur",
+//   umur: 65,
+// };
+
+// console.log(dataPersonal1);
+// console.log(dataPersonal2);
+// console.log(dataPersonal3);
+// console.log(dataPersonal4);
+// console.log(dataPersonal2.nama);
+
+// array of object
+let dataCaleg = [
+  { nama: "Joko Widodo", alamat: "Solo" },
+  { nama: "Puan Maharani", alamat: "Jakarta Barat" },
+  { nama: "Prabowo S", alamat: "Semarang" },
+];
+
+console.log(dataCaleg);
+console.log(dataCaleg[1].nama);
