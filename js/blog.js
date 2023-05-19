57b3f65648b5bb09e0563e704fc900fc367a3852
adhxabre <abeldustin06@gmail.com>
2023-05-19 11:27:35 +0700

    Day 4

--- js/blog.js
+++ js/blog.js
@@ -52,11 +52,73 @@
 // console.log(dataPersonal2.nama);
 
 // array of object
-let dataCaleg = [
-  { nama: "Joko Widodo", alamat: "Solo" },
-  { nama: "Puan Maharani", alamat: "Jakarta Barat" },
-  { nama: "Prabowo S", alamat: "Semarang" },
-];
+// let dataCaleg = [
+//   { nama: "Joko Widodo", alamat: "Solo" },
+//   { nama: "Puan Maharani", alamat: "Jakarta Barat" },
+//   { nama: "Prabowo S", alamat: "Semarang" },
+// ];
 
-console.log(dataCaleg);
-console.log(dataCaleg[1].nama);
+// console.log(dataCaleg);
+// console.log(dataCaleg[1].nama);
+
+let dataBlog = [];
+
+function addBlog(event) {
+  event.preventDefault();
+
+  let title = document.getElementById("input-blog-title").value;
+  let content = document.getElementById("input-blog-content").value;
+  let image = document.getElementById("input-blog-image").files;
+
+  // untuk membuat url gambar, agar tampil
+  image = URL.createObjectURL(image[0]);
+  console.log(image);
+
+  let blog = {
+    title,
+    content,
+    image,
+    postAt: "19 May 2023",
+    author: "Abel Dustin",
+  };
+
+  dataBlog.push(blog);
+  console.log(dataBlog);
+
+  renderBlog();
+}
+
+function renderBlog() {
+  document.getElementById("contents").innerHTML = "";
+
+  //   for (let index = 0; index < dataBlog.length; index++) {
+  //     console.log(index);
+  //   }
+
+  for (let index = 0; index < dataBlog.length; index++) {
+    document.getElementById("contents").innerHTML += `
+        <div class="blog-list-item">
+            <div class="blog-image">
+                <img src="${dataBlog[index].image}" alt="blog_img" />
+            </div>
+            <div class="blog-content">
+            <div class="btn-group">
+                <button class="btn-edit">Edit Post</button>
+                <button class="btn-delete">Delete Post</button>
+            </div>
+            <h1>
+                <a href="blog-detail.html" target="_blank"
+                >${dataBlog[index].title}</a
+                >
+            </h1>
+            <div class="detail-blog-content">
+                ${dataBlog[index].postAt} | ${dataBlog[index].author}
+            </div>
+            <p>
+                ${dataBlog[index].content}
+            </p>
+            </div>
+        </div>
+    `;
+  }
+}
