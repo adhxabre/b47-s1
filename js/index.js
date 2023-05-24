7d7d7ddee20558ccf656cb977db96bd157e1b28a
adhxabre <abeldustin06@gmail.com>
2023-05-24 10:25:00 +0700

    Day 6

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log("selamat pagi!");
+
+let hamburgerIsOpen = false;
+
+function openHamburger() {
+  let hamburgerNavContainer = document.getElementById(
+    "hamburger-nav-container"
+  );
+
+  // !hamburgerIsOpen > hamburgerIsOpen == false
+  // hamburgerIsOpen > hamburgerIsOpen == true
+  if (!hamburgerIsOpen) {
+    console.log(hamburgerIsOpen);
+    hamburgerNavContainer.style.display = "block";
+    hamburgerIsOpen = true;
+  } else {
+    console.log(hamburgerIsOpen);
+    hamburgerNavContainer.style.display = "none";
+    hamburgerIsOpen = false;
+  }
+}
